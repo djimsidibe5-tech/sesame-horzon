7ee9ccb3b8454d4ba4986358bf7ebae1ee008760
djimsidibe5-tech <djimsidibe5@gmail.com>
2026-07-02 18:14:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,17 @@ async def profil(request: Request):
 @app.get("/matiere", name="matiere")
 async def matiere(request: Request):
     return templates.TemplateResponse("matiere.html", {"request": request})
+    
+@app.get("/telecharger/{niveau}/{semestre}/{session}/{nom_fichier}", name="telecharger")
+async def telecharger(niveau: str, semestre: str, session: str, nom_fichier: str):
+    # On reconstruit le chemin vers le fichier sur le serveur Render
+    chemin_fichier = f"sesame-tech/static/documents/{niveau}/{semestre}/{session}/{nom_fichier}"
+   
+    # On vérifie que le fichier existe bien pour éviter les erreurs
+    if os.path.exists(chemin_fichier):
+        return FileResponse(chemin_fichier, media_type='application/pdf', filename=nom_fichier)
+    else:
+        return {"error": "Ce document n'existe pas encore."}
     # Données du créateur
     biographie = {
         "nom": "SIDIBE DJIM",
